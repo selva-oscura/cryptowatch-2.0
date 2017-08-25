9d35f5b05b2160d869d4c8a059f1481f5ff64790
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-24 20:13:13 -0700

    removed code using crypto-socket

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-cryptoSocket = require("crypto-socket")
 
 class App extends Component {
   componentWillMount(){
-    cryptoSocket.start();
-    cryptoSocket.start("bitfinex","ETHBTC");
-    cryptoSocket.start("bitmex","ETHBTC");
-    cryptoSocket.start("cex","ETHBTC");
-    setInterval(() => {
-        cryptoSocket.echoExchange()
-    },1000);
-}
+  }
   render() {
     return (
       <div className="App">
