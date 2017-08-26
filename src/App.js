26dd9178af497ff9de3d7320bec32fcdef1e5818
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-25 19:51:23 -0700

    subscribed to cryptocompare data and saved to state (imported societ.io-client & CCC library; added state and subscription to cryptocompare, saved unpacked current data to state;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import './App.css';
+import CCC from './utils/ccc-streamer-utilities.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: {},
+    };
+  }
+
   componentWillMount(){
+    let current = this.state.current;
+
+    // instantiate socket
+    const socket = io.connect('https://streamer.cryptocompare.com/');
+
+    // subscribe to current quotes
+    const subscriptions = ['5~CCCAGG~BTC~USD','5~CCCAGG~ETH~USD'];
+    socket.emit('SubAdd', {subs:subscriptions} );
+
+    // listen to socket & update state
+    socket.on("m", (message) => {
+      let messageType = message.substring(0, message.indexOf("~"));
+      let res = {};
+      if (messageType === CCC.STATIC.TYPE.CURRENTAGG) {
+        res = CCC.CURRENT.unpack(message);
+        if (res.PRICE) {
+          Object.keys(res).forEach(detail => console.log(detail, JSON.stringify(res[detail])))
+          current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] = res;
+        }
+        this.setState({current});
+      }
+    });
   }
   render() {
     return (
