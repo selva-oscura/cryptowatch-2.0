d4654f3c42c2242508b458c3ee8adf80708c42d0
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-27 18:17:38 -0700

    added more data (greater extremes); made Y coordinates negative to flip axis; reduced range and svg height to 200; imported styling;

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import './LineGraph.css';
 
 const LineGraph = () => {
-  let data = [[0, 12], [89, 9], [12, 50]];
+  // let data = [[0, 12], [89, 9], [12, 50]];
+  let data = [[0, 12], [6, 9], [12, 50], [18, 500]];
+  data = data.map(point => ([point[0], -point[1]]));
+  console.log('data', data);
   let xScale = d3.scaleLinear()
   .domain(d3.extent(data, ([x, y]) => x))
   .range([0, 400]);
 
   let yScale = d3.scaleLinear()
   .domain(d3.extent(data, ([x, y]) => y))
-  .range([0, 400]);
+  .range([0, 200]);
 
   const line = d3.line()
   .x(([x, y]) => xScale(x))
@@ -17,7 +21,7 @@ const LineGraph = () => {
   return (
     <div className="LineGraph">
       <p className="caption">ETH</p>
-      <svg width="400" height="400">
+      <svg width="400" height="200">
           <path style={{stroke: "grey", fill:"none"}}d={line(data)}></path>
       </svg>
     </div>
