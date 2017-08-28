8214663ee80ff64035a8bda1fd9d942db3f33c59
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-28 16:57:59 -0700

    added selected to parameters & used it to specify button class as currency-selected or currency-unselected

--- src/CurrencyButton.js
+++ src/CurrencyButton.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const CurrencyButton = ({currency}) => {
+const CurrencyButton = ({currency, selected}) => {
 	let keys = Object.keys(currency);
+	let buttonClass = selected.includes(keys[0]) ? "currency-selected" : "currency-unselected";
 	return (
 		<button
+			className={buttonClass}
 		>
 			{currency[keys[0]]} ({keys[0]})
 		</button>
