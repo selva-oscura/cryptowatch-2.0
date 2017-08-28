af379a4e3e0e1c9ad1f7a8b811bb3af4592c5582
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-27 23:15:53 -0700

    added fromCurrencies and toCurrencies (arrays of currencies & abbreviations for which conversions are available)

--- src/Currencies.js
+++ src/Currencies.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 const Currencies = () => {
+	const fromCurrencies = [
+		{"BTC":  	"Bitcoin"},
+		{"ETH": 	"Ethereum"},
+		{"LTC": 	"Litecoin"},
+		{"BCH": 	"Bitcoin Cash / BCC"},
+		{"DASH": 	"Digital Cash"},
+		{"EOS": 	"EOS"},
+		{"ETC": 	"Ethereum Classic"},
+		{"LSK": 	"Lisk"},
+		{"IOT": 	"IOTA"},
+		{"XMR": 	"Monero"},
+		{"NEO": 	"NEO"},
+		{"OMG": 	"OmiseGo"},
+		{"XRP": 	"Ripple"},
+		{"ZEC": 	"ZCASH"},
+	];
+	const toCurrencies = [
+		{"USD": 	"US Dollar"},
+		{"EUR": 	"Euro"},
+		{"GBP": 	"British Pound"}
+	];
 	return (
 		<div>
 			<p>Currencies here</p>
