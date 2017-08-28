66c7c757fec9fc7f39b533559ca074224315a971
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-27 18:07:24 -0700

    converted LineGraph to dumb component; added a couple more data points; reduced width/height from 500 to 400;

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
+const LineGraph = () => {
+  let data = [[0, 12], [89, 9], [12, 50]];
+  let xScale = d3.scaleLinear()
+  .domain(d3.extent(data, ([x, y]) => x))
+  .range([0, 400]);
 
+  let yScale = d3.scaleLinear()
+  .domain(d3.extent(data, ([x, y]) => y))
+  .range([0, 400]);
 
-class LineGraph extends Component {
-
-  render() {
-    let data = [[0, 12], [89, 9], [12, 50]];
-    let xScale = d3.scaleLinear()
-    .domain(d3.extent(data, ([x, y]) => x))
-    .range([0, 500]);
-
-    let yScale = d3.scaleLinear()
-    .domain(d3.extent(data, ([x, y]) => y))
-    .range([0, 500]);
-
-    
-    const line = d3.line()
-    .x(([x, y]) => xScale(x))
-    .y(([x, y]) => yScale(y))
-    return (
-      <div className="LineGraph">
-        <svg width="500" height="500">
-            <path style={{stroke: "black", fill:"none"}}d={line(data)}></path>
-        </svg>
-      </div>
-    );
-  }
+  const line = d3.line()
+  .x(([x, y]) => xScale(x))
+  .y(([x, y]) => yScale(y))
+  return (
+    <div className="LineGraph">
+      <p className="caption">ETH</p>
+      <svg width="400" height="400">
+          <path style={{stroke: "grey", fill:"none"}}d={line(data)}></path>
+      </svg>
+    </div>
+  );
 }
 
 export default LineGraph;
