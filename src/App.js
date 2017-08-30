fc75997c4798f11cee1fcea8c2a12bcd304f2cfd
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-29 22:46:24 -0700

    added subscription to current data when new currency is selected (created subscribeToCurrentQuotes (now called by bootstrapData and updateSelectedCurrencies)); moved currency subscriptions from componentWillMount to subscribeToCurrentQuotes (and replaced it with call to bootstrapData); some variable and variable name refactoring to make more clear/consistent;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import allCurrencies from './utils/currencies.js';
+import { allCurrencies } from './utils/currencies.js';
 import axios from 'axios';
 import io from 'socket.io-client';
 import CCC from './utils/ccc-streamer-utilities.js';
@@ -25,13 +25,16 @@ class App extends Component {
     this.updateSelectedCurrencies = this.updateSelectedCurrencies.bind(this);
   }
 
-  bootStrapHistoricalData(){
-    let selectedCurrencies = this.state.selectedCurrencies;
+  bootstrapData(){
+    let { selectedCurrencies } = this.state;
+    let subscriptions = [];
     selectedCurrencies.fromCur.forEach((f_cur) => {
       selectedCurrencies.toCur.forEach((t_cur) => {
         this.fetchHistoricalData(f_cur, t_cur);
+        subscriptions.push(`5~CCCAGG~${f_cur}~${t_cur}`);
       });
     });
+    this.subscribeToCurrentQuotes(subscriptions);
   }
 
   fetchHistoricalData(f_cur, t_cur){
@@ -58,6 +61,35 @@ class App extends Component {
     });
   }
 
+  subscribeToCurrentQuotes(subscriptions){
+    // get current (current quotes object) in state
+    let { current } = this.state;
+
+    // instantiate socket
+    const socket = io.connect('https://streamer.cryptocompare.com/');
+
+    // subscribe to current quotes
+    socket.emit('SubAdd', {subs:subscriptions} );
+
+    // listen to socket & update state
+    socket.on("m", (message) => {
+      let messageType = message.slice(0, message.indexOf("~"));
+      let res = {};
+      if (messageType === CCC.STATIC.TYPE.CURRENTAGG) {
+        res = CCC.CURRENT.unpack(message);
+
+        // check if response has all needed fields
+        if(res.LASTUPDATE && res.PRICE && res.FROMSYMBOL && res.TOSYMBOL){
+          // only update state.current if there is a change in price (or if this is first current data received)
+          if (current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] === undefined || (current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`].PRICE !== res.PRICE)){
+            current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] = res;
+            this.setState({current});
+          }
+        }
+      }
+    });
+  }
+
   updateSelectedCurrencies(clickedCurrency, fromOrTo){
     let selectedCurrencies = this.state.selectedCurrencies;
     if (selectedCurrencies[fromOrTo].includes(clickedCurrency)) {
@@ -69,15 +101,19 @@ class App extends Component {
       });
     } else {
       selectedCurrencies[fromOrTo].push(clickedCurrency);
+      let subscriptions = [];
       if(fromOrTo==="fromCur"){
         selectedCurrencies.toCur.forEach(t_cur => {
           this.fetchHistoricalData(clickedCurrency, t_cur);
+          subscriptions.push(`5~CCCAGG~${clickedCurrency}~${t_cur}`);
         });
       } else {
         selectedCurrencies.fromCur.forEach(f_cur => {
           this.fetchHistoricalData(f_cur, clickedCurrency);
+          subscriptions.push(`5~CCCAGG~${f_cur}~${clickedCurrency}`);
         });
       }
+      this.subscribeToCurrentQuotes(subscriptions);
     }
     this.setState({selectedCurrencies});
   }
@@ -90,50 +126,16 @@ class App extends Component {
   }
 
   componentWillMount(){
-    let { current, recent, selectedCurrencies, allCurrencies } = this.state;
-
-    this.bootStrapHistoricalData();
-
-    // instantiate socket
-    const socket = io.connect('https://streamer.cryptocompare.com/');
-
-    // subscribe to current quotes
-    const subscriptions = ['5~CCCAGG~BTC~USD','5~CCCAGG~ETH~USD'];
-    socket.emit('SubAdd', {subs:subscriptions} );
-
-    // listen to socket & update state
-    socket.on("m", (message) => {
-      let messageType = message.substring(0, message.indexOf("~"));
-      let res = {};
-      if (messageType === CCC.STATIC.TYPE.CURRENTAGG) {
-        res = CCC.CURRENT.unpack(message);
-
-        // update if provided data has all needed fields
-        if(res.LASTUPDATE && res.PRICE && res.FROMSYMBOL && res.TOSYMBOL){
-
-          // instantiate array for each currency pair in state.recent the first time data is received for that currency
-          if (recent[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] === undefined) {
-            recent[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] = [];
-          }
-          // only update state.current if there is a change in price (or if this is first current data received)
-          if(this.state.recent[`${res.FROMSYMBOL}-${res.TOSYMBOL}`].length === 0 || res.PRICE !== this.state.current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`].PRICE){
-              current[`${res.FROMSYMBOL}-${res.TOSYMBOL}`] = res;
-          }
-          // always add newest time & price data to state.recent
-          recent[`${res.FROMSYMBOL}-${res.TOSYMBOL}`].push([res.LASTUPDATE, res.PRICE]);
-
-          this.setState({current, recent});
-        }
-      }
-    });
+    this.bootstrapData();
   }
 
   render() {
-    let currentData = Object.keys(this.state.current),
-        historicalData = Object.keys(this.state.historical),
+    let {current, historical, selectedCurrencies} = this.state;
+    let currentKeys = Object.keys(current),
+        historicalKeys = Object.keys(historical),
         historicalCloseData = {};
-    historicalData.forEach(currency => {
-      historicalCloseData[`${currency}`] = this.state.historical[`${currency}`].map(point => ([point.time, point.close]))
+    historicalKeys.forEach(currency => {
+      historicalCloseData[`${currency}`] = historical[`${currency}`].map(point => ([point.time, point.close]))
     });
     return (
       <div className="App">
@@ -146,18 +148,18 @@ class App extends Component {
           <div className="App-body">
             
             <Currencies 
-              selectedFromCurrencies={this.state.selectedCurrencies.fromCur}
-              selectedToCurrencies={this.state.selectedCurrencies.toCur}
+              selectedFromCurrencies={selectedCurrencies.fromCur}
+              selectedToCurrencies={selectedCurrencies.toCur}
               updateSelectedCurrencies={this.updateSelectedCurrencies}
             />
 
             <div className="m-half s-all">
               {
-                currentData.map((current, i) => (
+                currentKeys.map((currencyPair, i) => (
                   <CurrentQuote
                     key={i}
-                    current={this.state.current[current]}
-                    yesterday={this.state.historical[current]}
+                    current={current[currencyPair]}
+                    yesterday={historical[currencyPair]}
                   />
                 ))
               }
@@ -165,7 +167,7 @@ class App extends Component {
 
             <div className="m-half s-all LineGraphs">
               {
-                this.state.selectedCurrencies.display.map((currencyPair, i) => (
+                selectedCurrencies.display.map((currencyPair, i) => (
                   <LineGraph
                     key={i}
                     currencyPair={currencyPair}
