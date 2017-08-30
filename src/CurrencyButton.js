365da36109264f99885b3c27b313984fa9e6e500
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-29 20:22:54 -0700

    completed update of selected currencies state in CurrencyButton (added updateSelectedCurrencies to component parameters, updated handleClick to trigger updateSelectedCurrencies)

--- src/CurrencyButton.js
+++ src/CurrencyButton.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './CurrencyButton.css';
 
-const CurrencyButton = ({currency, selected, fromOrTo}) => {
+const CurrencyButton = ({currency, selected, fromOrTo, updateSelectedCurrencies}) => {
+
 	const keys = Object.keys(currency);
+
 	const buttonClass = selected.includes(keys[0]) ? "currency-selected" : "currency-unselected";
+
 	const handleClick = () => {
-		console.log(keys[0], fromOrTo);
+		updateSelectedCurrencies(keys[0], fromOrTo);
 	}
+
 	return (
 		<button
 			className={buttonClass}
