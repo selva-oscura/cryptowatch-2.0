8b43a10bebb4b2dff4b17d74ab1d5b33c5f28198
Carol St.Louis <1.selva.oscura@gmail.com>
2017-08-29 20:27:45 -0700

    moved declaration of all supported currencies and generation of display order to currencies (now imported to be in state)

---
+++ src/utils/currencies.js
@@ -0,0 +1,38 @@
+const fromCurrencies = [
+      {"BTC":   "Bitcoin"},
+      {"ETH":   "Ethereum"},
+      {"LTC":   "Litecoin"},
+      {"BCH":   "Bitcoin Cash / BCC"},
+      {"DASH":  "Digital Cash"},
+      {"EOS":   "EOS"},
+      {"ETC":   "Ethereum Classic"},
+      {"LSK":   "Lisk"},
+      {"IOT":   "IOTA"},
+      {"XMR":   "Monero"},
+      {"NEO":   "NEO"},
+      {"OMG":   "OmiseGo"},
+      {"XRP":   "Ripple"},
+      {"ZEC":   "ZCASH"},
+    ],
+    toCurrencies = [
+      {"USD":   "US Dollar"},
+      {"EUR":   "Euro"},
+      {"GBP":   "British Pound"}
+    ],
+    pairingOrder = [];
+
+fromCurrencies.forEach(f_cur => {
+  toCurrencies.forEach(t_cur => {
+    let f_cur_abbr = Object.keys(f_cur),
+        t_cur_abbr = Object.keys(t_cur);
+    pairingOrder.push(`${f_cur_abbr[0]}-${t_cur_abbr[0]}`);
+  });
+});
+
+const allCurrencies = {
+  fromCurrencies,
+  toCurrencies,
+  pairingOrder,
+};
+
+export default allCurrencies;
