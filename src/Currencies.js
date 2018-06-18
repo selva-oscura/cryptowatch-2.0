c877842a7c831d28660c8195e5d889367bdfd2e9
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-06-17 19:10:34 -0700

    Currencies: added css classes to align currency types (crypto v real) to opposite sides of page and level with other components

--- src/Currencies.js
+++ src/Currencies.js
@@ -6,43 +6,41 @@ const Currencies = ({
   selectedFromCurrencies,
   selectedToCurrencies,
   updateSelectedCurrencies,
-}) => {
-  return (
-    <div>
-      <h2>Select Currencies:</h2>
-      <div>
-        <h3>Crypto-Currencies:</h3>
-        <div className="currency-list">
-          {allCurrencies.fromCurrencies.map((currency, i) => (
-            <CurrencyButton
-              key={i}
-              currency={currency}
-              selected={selectedFromCurrencies}
-              fromOrTo="fromCur"
-              updateSelectedCurrencies={updateSelectedCurrencies}
-            />
-          ))}
-        </div>
-      </div>
-      <div>
-        <h3>
-          Base Currencies:<br />
-          (price in terms of these currencies)
-        </h3>
-        <div className="currency-list">
-          {allCurrencies.toCurrencies.map((currency, i) => (
-            <CurrencyButton
-              key={i}
-              currency={currency}
-              selected={selectedToCurrencies}
-              fromOrTo="toCur"
-              updateSelectedCurrencies={updateSelectedCurrencies}
-            />
-          ))}
-        </div>
+}) => (
+  <div>
+    <h2>Select Currencies</h2>
+    <div className="full">
+      <h3 className="text-left">Crypto-Currencies:</h3>
+      <div className="currency-list">
+        {allCurrencies.fromCurrencies.map((currency, i) => (
+          <CurrencyButton
+            key={i}
+            currency={currency}
+            selected={selectedFromCurrencies}
+            fromOrTo="fromCur"
+            updateSelectedCurrencies={updateSelectedCurrencies}
+          />
+        ))}
       </div>
     </div>
-  );
-};
+    <div className="full">
+      <h3 className="text-right">
+        'Real' Currencies:<br />
+        (price in terms of these currencies)
+      </h3>
+      <div className="currency-list">
+        {allCurrencies.toCurrencies.map((currency, i) => (
+          <CurrencyButton
+            key={i}
+            currency={currency}
+            selected={selectedToCurrencies}
+            fromOrTo="toCur"
+            updateSelectedCurrencies={updateSelectedCurrencies}
+          />
+        ))}
+      </div>
+    </div>
+  </div>
+);
 
 export default Currencies;
