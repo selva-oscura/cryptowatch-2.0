2628b170bc2aa5c99295fe840b9029b7968d5fbc
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2018-06-18 20:52:40 -0700

    LineGraph: added text details (start/end dates & prices), modularized/reorganized variables/functions, switched to flexbox layout

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -4,27 +4,64 @@ import CCC from '../api/ccc-streamer-utilities.js';
 import './LineGraph.css';
 
 const LineGraph = ({ currencyPair, data }) => {
-  let min, max, numDays, currencySign;
-  min = Infinity;
-  max = -Infinity;
-  data.forEach(point => {
-    min = Math.min(min, point[1]);
-    max = Math.max(max, point[1]);
-  });
-  numDays = (data[data.length - 1][0] - data[0][0]) / (24 * 60 * 60);
-  currencySign = CCC.STATIC.CURRENCY.SYMBOL[currencyPair.slice(-3)];
-  data = data.map(point => [point[0], -point[1]]);
+  let minPrice = Infinity,
+    maxPrice = -Infinity,
+    minPriceDate,
+    maxPriceDate,
+    [startDate, startPrice] = data[0],
+    [endDate, endPrice] = data[data.length - 1],
+    numDays = (endDate - startDate) / (24 * 60 * 60),
+    currencySymbol;
 
+  // calculate minPrice & maxPrice price and their corresponding dates
+  data.forEach(point => {
+    if (point[1] < minPrice) {
+      minPrice = point[1];
+      minPriceDate = point[0];
+    }
+    if (point[1] > maxPrice) {
+      maxPrice = point[1];
+      maxPriceDate = point[0];
+    }
+  });
+
+  // format dates and prices
+  const formatDateFromTimestamp = timestamp => {
+    const date = new Date(timestamp * 1000);
+    return `${(date.getMonth() % 12) + 1}/${date.getDate()}/${String(
+      date.getFullYear()
+    ).slice(-2)}`;
+  };
+  const formatPrice = price =>
+    price.toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+  console.log('minPrice before', minPrice);
+  minPrice = formatPrice(minPrice);
+  console.log('minPrice after', minPrice);
+  maxPrice = formatPrice(maxPrice);
+  minPriceDate = formatDateFromTimestamp(minPriceDate);
+  maxPriceDate = formatDateFromTimestamp(maxPriceDate);
+  startPrice = formatPrice(startPrice);
+  endPrice = formatPrice(endPrice);
+  startDate = formatDateFromTimestamp(startDate);
+  endDate = formatDateFromTimestamp(endDate);
+  // numDays = (endDate - startDate) / (24 * 60 * 60);
+
+  // get currency symbol for real currency
+  currencySymbol = CCC.STATIC.CURRENCY.SYMBOL[currencyPair.slice(-3)];
+
+  // prepare data for d3
+  data = data.map(point => [point[0], -point[1]]);
   let xScale = d3
     .scaleLinear()
     .domain(d3.extent(data, ([x, y]) => x))
     .range([0, 400]);
-
   let yScale = d3
     .scaleLinear()
     .domain(d3.extent(data, ([x, y]) => y))
     .range([0, 200]);
-
   const lineChart = d3
     .line()
     .x(([x, y]) => xScale(x))
@@ -37,20 +74,41 @@ const LineGraph = ({ currencyPair, data }) => {
           <path style={{ stroke: 'grey', fill: 'none' }} d={lineChart(data)} />
         </svg>
         <div className="caption">
-          <p className="quote-normal">{currencyPair}</p>
-          <p className="quote-tiny">last {numDays} days</p>
-          <p className="quote-tiny">
-            {currencySign}
-            {min.toLocaleString('en-US', {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}{' '}
-            - {currencySign}
-            {max.toLocaleString('en-US', {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })}
-          </p>
+          <div className="flex-container" style={{ alignItems: 'flex-end' }}>
+            <div className="flex-item">
+              <p className="quote-normal" style={{ textAlign: 'left' }}>
+                {currencyPair}
+              </p>
+            </div>
+            <div className="flex-item">
+              <p
+                className="quote-tiny"
+                style={{ textAlign: 'right', verticalAlign: 'text-bottom' }}
+              >
+                (last {numDays} days)
+              </p>
+            </div>
+          </div>
+          <div className="flex-container">
+            <div className="flex-item">
+              <p className="quote-tiny" style={{ textAlign: 'left' }}>
+                {startDate}: {currencySymbol}
+                {startPrice}
+                <br />
+                {endDate}: {currencySymbol}
+                {endPrice}
+              </p>
+            </div>
+            <div className="flex-item">
+              <p className="quote-tiny" style={{ textAlign: 'right' }}>
+                low: {currencySymbol}
+                {minPrice} on {minPriceDate}
+                <br />
+                high: {currencySymbol}
+                {maxPrice} on {maxPriceDate}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
